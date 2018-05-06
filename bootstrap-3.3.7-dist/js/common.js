76b04ed0d21374aa26d4991ec27b68741a895b57
daguimu <daguimu@baiwang.com>
2018-05-06 23:48:29 +0800

    用户角色申请页面添加

--- bootstrap-3.3.7-dist/js/common.js
+++ bootstrap-3.3.7-dist/js/common.js
@@ -1,12 +1,24 @@
 function basefun(method, data, requestme, fun) {
+    var token_fa = $("#myphone", window.parent.document).val();
+    if (method != "userLogin" && token_fa == undefined) {
+        alert("您还没有登录");
+        window.location.href= "../../FrontSpare/login/index.html";
+    }
+    var token = token_fa == undefined ? null : sessionStorage.getItem("token:" + token_fa.split("-")[0]);
+    var header = {"token": token};
     $.post({
         url: "http://localhost:9999/" + method,
         type: requestme,
+        headers: header,
         data: JSON.stringify(data),
         dataType: "json",
         Origin: "http://localhost:9999/",
         contentType: "application/json",
         success: function (resultdata) {
+            if (resultdata.message == "您还没有登录") {
+                alert("您还没有登录");
+                window.location.href= "../../FrontSpare/login/index.html";
+            }
             fun(resultdata);
         }
     });
@@ -81,10 +93,16 @@ $(document).on('mouseup', '.paginationjs-pages li', function () {
 
 
 function numberConvert(num) {
-    var len=(num+"").length;
-    var result="";
-    for(var i=0;i<8-len;i++){
-        result+="0";
+    var len = (num + "").length;
+    var result = "";
+    for (var i = 0; i < 8 - len; i++) {
+        result += "0";
     }
-    return result+num;
+    return result + num;
+}
+
+function logout() {
+    var token_fa = $("#myphone", window.parent.document).val();
+    sessionStorage.removeItem("token:" + token_fa.split("-")[0]);
+    self.location = "../../FrontSpare/login/index.html";
 }
